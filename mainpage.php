0bc2e8736f701e9ea659ad217e32393ffe6af0ec
SimonKritzinger <31211206+SimonKritzinger@users.noreply.github.com>
2017-09-30 13:26:42 +0200

    Add files via upload

---
+++ mainpage.php
@@ -0,0 +1,16 @@
+
+<link rel="stylesheet" href="bootstrap.css">
+<link rel="stylesheet" href="mainpage.css">
+<div class="row">
+		<div class="col-md-5 col-md-offset-1">
+			<div class="button" id="projects"><p class="buttontext">Projects</p></div>
+		</div>
+		<div class="col-md-5">
+				<div class="button" id="users"><p class="buttontext">Users</p></div>
+		</div>
+		</div>
+<div class="row">
+	<div class="col-md-10 col-md-offset-1">
+				<div class="button" id="timeline"><p class="buttontext">Timeline</p></div>
+	</div>		
+		</div>
